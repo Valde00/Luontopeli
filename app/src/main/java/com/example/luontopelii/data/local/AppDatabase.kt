40e3630ce5ef01b29d72e5826c372113b5390dc1
Valtteri Rehu <90039969+Valde00@users.noreply.github.com>
2026-04-19 15:52:37 +0300

    Revert "aa"
    This reverts commit db744e85067b1ab990e4c4de88c9a6124f1a1f89.

--- app/src/main/java/com/example/luontopelii/data/local/AppDatabase.kt
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank. It was created in the wrong directory (typo in path).
-// The real AppDatabase implementation is in com/example/luontopeli/data/local/AppDatabase.kt
-
